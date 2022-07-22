7bfb93a19dd6359b89f55cf6b95a6bd2f61cb702
sundeepbasak <67704674+sundeepbasak@users.noreply.github.com>
2022-07-23 00:22:53 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="h-screen bg-gray-900 text-gray-100">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
